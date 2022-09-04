c8becb3235e0f106a75afcd449889a510ee28458
supplepentan <pentagramog@gmail.com>
2022-09-04 21:13:49 +0900

    first commit

---
+++ src/components/AreaMain.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import ImportingPdfFiles from './Fabric/ImportingPdfFiles.vue';
+</script>
+<template>
+  <div>
+    <ImportingPdfFiles />
+  </div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/Fabric/ImportingPdfFiles.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+    <h1>Importing PDF Files</h1>
+</template>
